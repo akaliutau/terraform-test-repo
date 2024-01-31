53fb559a873dce01e683ec2ce2a2589536dfe77e
Aliaksei Kaliutau <aliaksei.kaliutau@gmail.com>
2024-01-31 12:28:49 +0000

    added a simple resource

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 0.15"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
+resource "local_file" "text_file" {
+    filename = "sonet_18.txt"
+    content     = <<-EOT
+	Shall I compare thee to a summer’s day?
+	Thou art more lovely and more temperate:
+	Rough winds do shake the darling buds of May,
+	And summer’s lease hath all too short a date;
+	Sometime too hot the eye of heaven shines,
+	And often is his gold complexion dimm'd;
+	And every fair from fair sometime declines,
+	By chance or nature’s changing course untrimm'd;
+	But thy eternal summer shall not fade,
+	Nor lose possession of that fair thou ow’st;
+	Nor shall death brag thou wander’st in his shade,
+	When in eternal lines to time thou grow’st:
+   	  So long as men can breathe or eyes can see,
+   	  So long lives this, and this gives life to thee.
+    EOT
+}
